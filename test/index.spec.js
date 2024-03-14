6a7fd2881d865b58a291b9e9d13e3e9ad91c0573
Manuel Medina <mmedina@ub.edu.bz>
2024-03-14 15:51:18 -0600

    Add files via upload

---
+++ test/index.spec.js
@@ -0,0 +1,37 @@
+const assert = require("assert");
+const {
+  initDB,
+  createTable,
+  dropTable,
+  insertUser,
+  getUsersByName,
+  closeDBPool,
+} = require("../src/index.js");
+
+describe("Sqlite", function () {
+  let db;
+  before((done) => {
+    db = initDB(':memory:');
+    db.serialize(async function() {
+      await dropTable(db);
+      await createTable(db);
+      done();
+    });
+  });
+
+  it("Should insert and fetch a user", async () => {
+    const name = "mocha";
+    const name2 = "manuel";
+    const email = "mocha@test.com";
+    const email2 = "mister2@test.com";
+    const middle = "jose";
+
+    await insertUser(db, name, email);
+    const user = await getUsersByName(db, name);//test actual
+    assert.deepEqual(user, [{ id: 1, name, email }]);//test expected
+  });
+
+  after(() => {
+    closeDBPool(db);
+  });
+});
